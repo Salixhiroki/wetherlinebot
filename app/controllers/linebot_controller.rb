7b63ec89465cb41a68ae187faf337386200b2b1a
Tobita_Hiroki <salix.hiro@gmail.com>
2020-05-28 13:57:59 +0000

    linebot

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -90,29 +90,10 @@ class LinebotController < ApplicationController
   
   def template3
     {
-  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-  "type": "message",
-  "mode": "active",
-  "timestamp": 1462629479859,
-  "source": {
-    "type": "user",
-    "userId": "U4af4980629..."
-  },
-  "message": {
-    "id": "325708",
-    "type": "text",
-    "text": "お前はバカか",
-    "emojis": [
-      {
-        "index": 14,
-        "length": 6,
-        "productId": "5ac1bfd5040ab15980c9b435",
-        "emojiId": "001"
-      }
-    ]
-  }
-}
-end
+      "type": "text",
+      "text": "Hello, world"
+    }
+  end
   
   
 
